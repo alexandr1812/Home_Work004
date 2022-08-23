d0d6add250f18682eae92bcf667614df22866ce9
Alexander <jayero@mail.ru>
2022-08-23 05:15:07 +0300

    done

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Привествую дорогой пользователь");
+Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int ExponentiationNumberB(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = result * numA;
+    }
+    return result;
+}
+
+int num = ExponentiationNumberB(numA, numB);
+Console.WriteLine($"Answer: {num}"); 
