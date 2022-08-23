188c5eff270bde8350dbd2e09bd0dc839b29536d
Alexander <jayero@mail.ru>
2022-08-23 05:15:28 +0300

    done2

---
+++ task27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int numberA)
+{
+    int result = 0;
+    int SumPlus = 0;
+    int Plus = Convert.ToString(numberA).Length;
+
+    for (int i = 0; i < Plus; i++)
+    {
+        SumPlus = numberA - numberA % 10;
+        result = result + (numberA - SumPlus);
+        numberA = numberA / 10;
+    }
+    return result;
+}
+int additionNumbers = SumNumbers(numberA);
+Console.WriteLine($"Answer -> {additionNumbers}");
