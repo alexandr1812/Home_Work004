3c10a406b59c89e446873a1f32a06247c1753649
Alexander <jayero@mail.ru>
2022-08-23 05:15:41 +0300

    done3

---
+++ task29/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] array = new int[8];
+
+void FillArray(int[] elements)
+{
+    int length = elements.Length;
+    int index = 0;
+    while (index < length)
+    {
+        elements[index] = new Random().Next(1, 100);
+         index++;
+    }
+
+}
+
+void PrintArray(int[] el)
+{
+    int count = el.Length;
+    int position = 0;
+    while(position < count)
+    {
+      Console.WriteLine(el[position]);
+        position++;
+    }
+}
+
+
+FillArray(array);
+PrintArray(array);
